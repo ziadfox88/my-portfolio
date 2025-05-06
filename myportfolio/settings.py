78f7d95cfee8cef65a0da3451eb70afb70604ace
Ziad Ali <ziadfox88@gmail.com>
2025-05-07 01:55:53 +0300

    remove cloudnary

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -145,8 +145,8 @@ STATIC_URL = '/static/'
 
 
 
-MEDIA_URL = '/staticfiles/'
-MEDIA_ROOT = BASE_DIR / 'staticfiles/img'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 STATICFILES_DIRS = [
