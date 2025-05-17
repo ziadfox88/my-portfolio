046a7b2582dd98ac35eb9fa8e39fc0c7d4702f76
Ziad Ali <ziadfox88@gmail.com>
2025-05-17 16:16:45 +0300

    new

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import cloudinary
-import cloudinary.uploader
-from cloudinary.utils import cloudinary_url
+# import cloudinary
+# import cloudinary.uploader
+# from cloudinary.utils import cloudinary_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
